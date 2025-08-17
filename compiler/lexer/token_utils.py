0bb2cebdf1999e0881e7b529edd87756adace475
Luis Diss <1luisdiss@gmail.com>
2025-08-18 00:24:36 +0100

    Made all token names upper case. Renamed number and string tokens. Removed keyword token. Moved keywords set outside of is key_word. Changed create token to recognise enum names instead of values

--- compiler/lexer/token_utils.py
+++ compiler/lexer/token_utils.py
@@ -1,23 +1,40 @@
 from enum import Enum, auto
 
+#tokens that lexer.py can recognise
 class TokenTypes(Enum):
-    keyword = auto()
     ID = auto()
-    number_literal = auto()
-    string_literal = auto()
-    op = auto()
-    punctuation = auto()
-    whitespace = auto()
+    NUMBER = auto()
+    STRING = auto()
+    OP = auto()
+    PUNCTUATION  = auto()
+    WHITESPACEE = auto()
 
-def is_keyword(word):
-    keywords = set(["func", "struct", "if", "elif", "else", "self", "gt", "lt", "ge", "le", "eq", "return"])
+keywords = set([    
+    "func",
+    "assign",
+    "if",
+    "else",
+    "return",
+    "call",
+    "gt",
+    "lt",
+    "ge",
+    "le",
+    "eq",
+    "ne",
+    "true",
+    "false"
+    ])
+
+#used to see which parsed IDs are actually keywords
+def is_keyword(word, keywords):
     if word in keywords:
         return True
 
 def create_token(token_type, token_value, tokens):
-    if (token_type == TokenTypes.ID.value and is_keyword(token_value)) or token_type == TokenTypes.op.value or token_type == TokenTypes.punctuation.value:
+    if (token_type == TokenTypes.ID.name and is_keyword(token_value, keywords)) or token_type == TokenTypes.OP.name or token_type == TokenTypes.PUNCTUATION.name:
         tokens.append((token_value, token_value))
-    elif token_type == TokenTypes.whitespace.value:
+    elif token_type == TokenTypes.WHITESPACEE.name:
         pass
     else:
         tokens.append((token_type, token_value))
