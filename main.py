a5f9aacd30a5b16bed06352686ab5ea5d26b74f4
Luis Diss <1luisdiss@gmail.com>
2026-03-05 23:18:31 +0000

    modified sample program

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from compiler.lexer.lexer import lexer
 from compiler.parser.parser import parser
-from compiler.parser.parser_utils import print_children
+from compiler.parser.parser_utils import ASTPrinter
+from compiler.sa.semantic_analyser import semantic_analyser
 
-input = "assign a = 2 + 2 3 + 4 call hello(5+5) func fol(a, b | assign r=5, assign e=2+2){2+2}"
+input = """
+func fol(a){
+    return a+2*3
+}
+call fol(1)
 
+if 1 lt 2 gt 3{
+    assign b = "hello world!"
+}
+"""
+    
 #lexing
 tokens = lexer(input)
 print(f'Tokens:\n\n{tokens}\n')
@@ -11,5 +21,10 @@ print(f'Tokens:\n\n{tokens}\n')
 #parsing
 tokens.append(("$", "$"))
 root = parser(tokens)
+
+#semantic analysis + code gen
+semantic_analyser(root)
+
 print('Syntax tree:\n')
-print_children(root)
+printer = ASTPrinter()
+printer.visit(root)
